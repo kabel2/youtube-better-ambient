40fa556b68071112f325d1b4634a747ca01bb124
Marius <fettestier@ok.de>
2025-05-31 00:58:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# youtube-better-ambient
-Improved ambient mode for YouTube videos
+# youtube-better-ambient  
+> :warning: WIP
+
+**Enhanced Ambient Mode for YouTube**  
+youtube-better-ambient is a browser extension that enhances YouTube’s ambient mode by placing a blurred version of the video in the background and making various UI elements transparent for a more immersive experience.
+
+![Streit](streit.webp)
+
+
+## Possible Features (Planned / Ideas)
+- Add settings to control blur intensity, opacity, and toggle the extension on/off
+- Publish extension on Chrome and Firefox extension stores
+- Use GitHub Actions for automated builds
+- Render blur using OpenGL with linear Gaussian blur (via multiple box blurs)
+- Automatically disable blur in fullscreen mode to reduce resource usage
---
+++ main.js
@@ -0,0 +1,111 @@
+const getVideo = () => document.querySelector('video-stream.html5-main-video, video');
+
+let video = getVideo();
+let canvas, ctx;
+let sourceX, sourceY, sourceW, sourceH;
+let aspectRatio = window.innerWidth / window.innerHeight;
+
+const calculateSourceRect = () => {
+    let videoW = video.videoWidth;
+    let videoH = video.videoHeight;
+    aspectRatio = window.innerWidth / window.innerHeight;
+
+    if (videoW / videoH > aspectRatio) {
+        sourceW = aspectRatio * videoH;
+        sourceX = (videoW - sourceW) / 2;
+        sourceY = 0;
+        sourceH = videoH;
+    } else {
+        sourceH = videoW / aspectRatio;
+        sourceY = (videoH - sourceH) / 2;
+        sourceX = 0;
+        sourceW = videoW;
+    }
+}
+
+const drawToCanvas = () => {
+    ctx.drawImage(
+        video,
+        sourceX, sourceY, sourceW, sourceH,
+        0, 0, window.innerWidth, window.innerHeight
+    );
+    video.requestVideoFrameCallback(drawToCanvas);
+}
+
+const initialize = () => {
+    canvas = document.createElement('canvas');
+    canvas.style.cssText = 'top:0;left:0;bottom:0;right:0;position:fixed;width:100vw;height:100vh;z-index:-1;filter:blur(10px) opacity(0.5);';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+
+    const defaultColor = '#12121252';
+
+    document.documentElement.style.setProperty('--yt-spec-general-background-a', '#18181873');
+    document.documentElement.style.setProperty('--yt-spec-brand-background-primary', 'rgba(24,24,24,.45)');
+    document.documentElement.style.setProperty('--ytd-searchbox-background', defaultColor);
+    document.documentElement.style.setProperty('--yt-spec-brand-background-solid', defaultColor);
+
+    const setPropertyIfExists = (selector, propertyName, value) => {
+        const element = document.querySelector(selector);
+        if (element) element.style.setProperty(propertyName, value, 'important');
+    }
+
+    setPropertyIfExists('ytd-app', 'background-color', 'transparent');
+    setPropertyIfExists('.ytSearchboxComponentInputBoxDark', 'border-color', 'rgba(18, 18, 18, 0.1)');
+    setPropertyIfExists('#masthead', 'backdrop-filter', 'blur(80px)');
+    setPropertyIfExists('#masthead', 'background-color', 'rgba(0,0,0,0.1)');
+    setPropertyIfExists('#container', 'background-color', 'rgba(0,0,0,0.3)');
+
+    document.querySelectorAll('#voice-search-button, .ytSearchboxComponentInputBoxDark, #background.ytd-masthead').forEach((element) => {
+        element.style.backgroundColor = defaultColor;
+    });
+
+    const chatButtonSelector = 'ytd-button-renderer.style-scope.ytd-live-chat-frame';
+
+    const observer = new MutationObserver((records) => {
+        if(document.querySelector(chatButtonSelector)) {
+            document.querySelector(chatButtonSelector).style.backgroundColor = defaultColor;
+            observer.disconnect(document);
+        }
+    });
+
+    setTimeout(() => {
+        observer.disconnect(document);
+    }, 5000);
+
+    observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
+
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        calculateSourceRect();
+    });
+
+    video.addEventListener('loadedmetadata', calculateSourceRect);
+
+    video.addEventListener('resize', calculateSourceRect);
+
+    calculateSourceRect();
+    drawToCanvas();
+}
+
+const detectVideoAndInit = () => {
+    video = getVideo();
+    if (video?.src) {
+        initialize();
+        return;
+    }
+
+    let observer = new MutationObserver(() => {
+        video = getVideo();
+        if (video?.src) {
+            observer.disconnect();
+            initialize();
+        }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+}
+
+detectVideoAndInit();
